d96149f756a2593ccf83af3f4eaaace689b9db1d
Wilton24 <cbasher24@gmail.com>
2023-09-12 00:22:41 +0800

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+  productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+  quantity: 3
+},{
+  productId: 'a82c6bac-3067-4e68-a5ba-d827ac0be010',
+  quantity: 1
+}];
 
 export function addToCart(productId){
   let matchingItem;
