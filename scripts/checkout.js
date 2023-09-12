4c5fae9d9492064013c9e853a96383085f8fadfa
Wilton24 <cbasher24@gmail.com>
2023-09-12 14:13:57 +0800

    Added a delete to cart function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { products } from "../data/products.js";
 import { formatCurrency } from './utils/money.js';
 
@@ -41,7 +41,7 @@ cart.forEach(cartItem=>{
         <span class="update-quantity-link link-primary">
           Update
         </span>
-        <span class="delete-quantity-link link-primary">
+        <span class="delete-quantity-link link-primary" data-product-id="${matchingProduct.id}">
           Delete
         </span>
       </div>
@@ -97,3 +97,11 @@ cart.forEach(cartItem=>{
 });
 
 orderSummary.innerHTML = cartSummaryHTML;
+
+const deleteItem = document.querySelectorAll('.delete-quantity-link');
+deleteItem.forEach(item=>{
+  item.addEventListener('click', ()=>{
+    const productId = item.dataset.productId;
+    removeFromCart(productId);
+  })
+}) 
